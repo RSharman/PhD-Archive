f29f7ecba210f8669a1a0c73fd45e569dffed284
Rebecca Sharman <lpxrs@nottingham.ac.uk>
2011-03-25 11:16:48 +0000

    2dfft: commit, before I delete all the test code. Also added radialProfile and radial_data

--- Image Statistics/2dfft.py
+++ Image Statistics/2dfft.py
@@ -4,8 +4,9 @@ from psychopy import colorFunctions, core, visual, event
 from scipy import fftpack
 import scipy, Image, copy, pylab
 import numpy as np
+import radialProfile
 
-picture = np.array(Image.open("WhiteDot.jpg").transpose(Image.FLIP_TOP_BOTTOM))/127.5-1
+picture = np.array(Image.open("Pansy512.jpg").transpose(Image.FLIP_TOP_BOTTOM))/127.5-1
 
 dklPicture = colorFunctions.rgb2dklCart(picture, conversionMatrix=None)
 dklPicture = np.array(dklPicture)
@@ -18,45 +19,76 @@ fftLum = fftpack.fft2(lum)
 fftLm = fftpack.fft2(lm)
 fftS = fftpack.fft2(s)
 
-n = lum.size
-timestep = 0.1
-fftLum = fftpack.fftfreq(n, d=timestep)
-fftLum = np.array(fftLum)
+#fftLumAmp = fftLum.real
+#fftLumPhase = fftLum.imag
+
+F2 = fftpack.fftshift(fftLum)
+
+#Not sure why this is squared www.astrobetter.com/fourier-transforms-of-images-in-python/
+#Or why a log is take before plotting, from the same source
+pylab.figure(1)
+pylab.clf()
+F3 = abs(F2)**2
+pylab.imshow(np.log10(F3))
+
+#Calculating the azimuthally average 1D power spectrum
+A1 = radialProfile.azimuthalAverage(F3)
+pylab.figure(2)
+pylab.clf()
+pylab.semilogy(A1)
+pylab.xlabel('Spatial Frequency')
+pylab.ylabel('Power Spectrum')
+
+
+
+
+#fftLumMag = abs(fftLum)**2
+#
+#fftLumMag = abs(fftpack.fftshift(fftLumMag))
+#fftLumMagNew = abs(fftpack.fftshift(fftpack.fft2(fftLumMag)))
+#
+#pylab.imshow(fftLumMag)
+#
+#
+#pylab.imshow(fftLumMagNew)
+#pylab.imshow(picture)
+#pylab.xlim(120,130)
+#pylab.ylim(120,130)
+#pylab.pink()
+pylab.show()
+
+#print fftLumMag[:,0]
+
+#centre=[]
+#centre2=[]
+#xrange = np.arange(75, 437)
+#for n in xrange:
+#
+#    centre.append(fftLumMag[:,n])
+#    centre2.append(centre[n,:])
+#
+#print centre2.shape
+
+
+#print np.min(fftLum)
+#print np.max(fftLum)
+
+#fftLum = (((fftLum +0.000850091991337)/259005.5678)*2)-1
 
 #fftLum = np.reshape(fftLum(512,512))
 
-#print fftLum
-print fftLum.shape
+#print fftLumMag
 
-#fftLum = np.reshape(fftLum, -1)
-
-#magnitudeLum = []
-#counter = 0
-#for n in fftLum:
-#    mag1 = np.sqrt((fftLum[counter])*(fftLum[counter]))
-#    counter += 1
-#    if counter>262143:
-#        counter -=1
-#    mag2 = np.sqrt((fftLum[counter])*(fftLum[counter]))
-#    mag = mag1+mag2
-#    magnitudeLum.append(mag)
-
-#print counter
-#magnitudeLum = np.array(magnitudeLum)
-#print magnitudeLum.shape
+#pylab.plot(fftLumMag)
+#pylab.ylim(0, 1000)
+#pylab.show()
 #
-#magnitudeLum = np.reshape(fftLum, (512,512))
-
-#print magnitudeLum
-    
-
-
-myWin = visual.Window(size=(1024, 768), monitor = 'testMonitor', units = 'degs',
-                fullscr=False, allowGUI=True)
-
-img = visual.PatchStim(myWin, tex=fftLum, sf=(1/10.0), size=20.0)
-img.draw()
-myWin.flip()
-
-junk = event.waitKeys()
+#myWin = visual.Window(size=(1024, 768), monitor = 'testMonitor', units = 'degs',
+#                fullscr=False, allowGUI=True)
+#
+#img = visual.PatchStim(myWin, tex=fftLumMag, sf=(1/20.0), size=20.0)
+#img.draw()
+#myWin.flip()
+#
+#junk = event.waitKeys()
 
